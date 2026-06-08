ff0a7c51c4fce2279aaea9bf19188bdd4476e069
Charles Wyner <charliewyner@gmail.com>
2026-06-08 12:07:49 -0400

    completed exercises 1.3.-1.5.

---
+++ part1/practice.js
@@ -0,0 +1,11 @@
+function product(a, b) {
+    return a * b
+}
+
+const result = product(2, 6)
+console.log('hello')
+console.log(result)
+
+const result2 = product(1, 5)
+console.log(result2)
+// result is now 12
