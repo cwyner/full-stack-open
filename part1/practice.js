1780badfd252155cf097fc2ea70b836e43b0d442
Charles Wyner <charliewyner@gmail.com>
2026-06-16 16:06:47 -0400

    completed Exercise 1

--- part1/practice.js
+++ part1/practice.js
@@ -1,11 +1,1 @@
-function product(a, b) {
-    return a * b
-}
-
-const result = product(2, 6)
-console.log('hello')
-console.log(result)
-
-const result2 = product(1, 5)
-console.log(result2)
-// result is now 12
+console.log(0.1 + 0.2); // 0.30000000000000004
---
+++ part1/practice/src/App.jsx
@@ -0,0 +1,32 @@
+// Currently at Part 1.d: Event Handling
+
+import { useState } from "react"
+
+const Display = props => <div>{props.value}</div>
+
+const Button = (props) => (
+  <button onClick={props.onClick}>
+    {props.text}
+  </button>
+)
+
+const App = () => {
+  const [value, setValue] = useState(10)
+
+
+  const setToValue = (newValue) => {
+    console.log('value now', newValue)  // print the new value to console
+    setValue(newValue)
+  }
+
+  return (
+    <div>
+      <Display value={value} />
+      <Button onClick={() => setToValue(1000)} text="thousand" />
+      <Button onClick={() => setToValue(0)} text="reset" />
+      <Button onClick={() => setToValue(value + 1)} text="increment" />
+    </div>
+  )
+}
+
+export default App
